443e4c884cc4848ceb99db98e433b45fd71d7efc
opensourcemechanic <brian.nitz@gmail.com>
2016-11-22 20:59:19 +0000

    Fixed comments, removed dead code, minor format changes

--- LanternDHT_delta/LanternDHT_delta.ino
+++ LanternDHT_delta/LanternDHT_delta.ino
@@ -1,22 +1,20 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  Lantern DHT
+  This sketch was written for Science hack day at the Tog Maker 
+  space in Dublin, Ireland. 20 November 2016. 
+  It it based on ladyada's public domain DHTtester. 
+  This sketch uses uses only the temperature 
+  from the DHT temperature and humidity sensor. 
 
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
+  The DHT sensor is slow so we only read it every 64 loop iterations
+   
+  Modified 22 November 2016 by Brian Nitz
+  
   This example code is in the public domain.
-  HDT22
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
+*/
 
  #include "DHT.h"
  #define DHTTYPE DHT11   // DHT 11
-
  #define DHTPIN 2 // what digital pin we're connected to
 
 int RED_LED = 12;
@@ -26,24 +24,21 @@ int BLUE_ON = 20;
 
 const int BLINK_MIN = 1;
 const int BLINK_MAX = 20;
-
 float temp_c = 0.0;
 float prev_temp_c = 0.0;
-
+int loop_count = 0;
 
 DHT dht(DHTPIN, DHTTYPE);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
+  // initialize red and green LED pins as outputs.
   pinMode(RED_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
-  // Serial.begin(9600);
-  //Serial.println("DHTxx test!");
+
   dht.begin();
   temp_c = dht.readTemperature();
   prev_temp_c = temp_c;
-  temp_int_c = int(temp_c * TEMP_SCALE);
   loop_count = 0;
 }
 
@@ -54,7 +49,6 @@ void loop() {
       if (isnan(temp_c)) {
           return;
       }
-      temp_int_c = int(temp_c * TEMP_SCALE);
       if (temp_c > prev_temp_c) {
           digitalWrite(RED_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
           digitalWrite(BLUE_LED, LOW);   // turn the LED on (HIGH is the voltage level)
@@ -67,6 +61,4 @@ void loop() {
   } 
   loop_count ++;
   
-
-
 }
